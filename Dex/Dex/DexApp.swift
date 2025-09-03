addc803fc0f3a7617135829c378150e9a6320fea
Sergio J <sergiojoel@MacBook-Air-de-Sergio.local>
2025-09-03 17:04:20 -0600

    Project created, Resources brought in.

---
+++ Dex/Dex/DexApp.swift
@@ -0,0 +1,20 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by Sergio J. on 03/09/25.
+//
+
+import SwiftUI
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
